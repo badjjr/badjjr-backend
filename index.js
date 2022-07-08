16b6f83fc865a9a34d83cef65446fa267f105ff9
rhsagar145 <>
2022-07-08 19:37:43 -0400

    quiz model added

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 //=============================================================================
 // Basic Config
 //=============================================================================
-//Import express
 const express = require('express');
-//Instantiate express
+// instantiate express
 const app = express();
+//Require cors
+const cors = require('cors');
 //Set server port
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 3000);
 
 //=============================================================================
 // Middleware
 //=============================================================================
-// Parses key value pairs in request
-app.use(express.urlencoded({ extended: true }));
-// Converts json strings to the an object and attaches it to req.body
+// `express.json` parses application/json request data and
+//  adds it to the request object as request.body
 app.use(express.json());
-// Use cors package to allow connections from all domains
-const cors = require('cors');
+// `express.urlencoded` parses x-ww-form-urlencoded request data and
+//  adds it to the request object as request.body
+app.use(express.urlencoded({ extended: true }));
+//Enable cross-origin resource sharing (aka let other websites make requests to our api)
 app.use(cors());
-
 //=============================================================================
 // ROUTES
 //=============================================================================
-// Redirect any requests to the homepage to quizzes API
+// Redirect
 app.get('/', (req, res) => {
-	res.redirect('/api/quizzes');
+	res.redirect('/gifs');
 });
+/* START CONTROLLERS HERE */
+const giphController = require('./controller/giphController.js');
+//Delegate all requests to giphs to the controller
+app.use('/gifs', giphController);
 
-//quizzes controller
-const quizzesController = require('./controllers/quizzesController');
-app.use('/api/quizzes/', quizzesController);
+const userController = require('./controller/userController.js');
+app.use('/users', userController);
 
-//questions controller
-const questionsController = require('./questions/questionsController');
-app.use('/api', questionsController);
-
-// // The catch all for handling errors
-// const { handleErrors } = require('./middleware/custom_errors');
-// app.use(handleErrors);
-
+/* END CONTROLLERS HERE */
 //=============================================================================
 // START SERVER
 //=============================================================================
 app.listen(app.get('port'), () => {
-	console.log(`✅ Listening on port ${app.get('port')}`);
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
 });
--- models/Quiz.js
+++ models/Quiz.js
@@ -1,5 +1,14 @@
 const mongoose = require('../db/connection');
 
-const quizSchema = new mongoose.Schema({});
+const quizSchema = new mongoose.Schema(
+	{
+		title: String,
+		numberOfQuestions: Number,
+		category: String,
+		questions: [questionSchema],
+		creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	},
+	{ timestamps: true }
+);
 
-module.exports = mongoose.model('User', quizSchema);
+module.exports = mongoose.model('Quiz', quizSchema);
