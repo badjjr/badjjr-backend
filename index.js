c0f1b6cfaf6731f66e19f8e928b8c5614259d6ef
rhsagar145 <>
2022-07-08 19:50:34 -0400

    index fixed

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 //=============================================================================
 // Basic Config
 //=============================================================================
+//Import express
 const express = require('express');
-// instantiate express
+//Instantiate express
 const app = express();
-//Require cors
-const cors = require('cors');
 //Set server port
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
 
 //=============================================================================
 // Middleware
 //=============================================================================
-// `express.json` parses application/json request data and
-//  adds it to the request object as request.body
-app.use(express.json());
-// `express.urlencoded` parses x-ww-form-urlencoded request data and
-//  adds it to the request object as request.body
+// Parses key value pairs in request
 app.use(express.urlencoded({ extended: true }));
-//Enable cross-origin resource sharing (aka let other websites make requests to our api)
+// Converts json strings to the an object and attaches it to req.body
+app.use(express.json());
+// Use cors package to allow connections from all domains
+const cors = require('cors');
 app.use(cors());
+
 //=============================================================================
 // ROUTES
 //=============================================================================
-// Redirect
+// Redirect any requests to the homepage to quizzes API
 app.get('/', (req, res) => {
-	res.redirect('/gifs');
+	res.redirect('/api/quizzes');
 });
-/* START CONTROLLERS HERE */
-const giphController = require('./controller/giphController.js');
-//Delegate all requests to giphs to the controller
-app.use('/gifs', giphController);
 
-const userController = require('./controller/userController.js');
-app.use('/users', userController);
+//quizzes controller
+const quizzesController = require('./controllers/quizzesController');
+app.use('/api/quizzes/', quizzesController);
 
-/* END CONTROLLERS HERE */
+//questions controller
+const questionsController = require('./controllers/questionsController');
+app.use('/api', questionsController);
+
+// // The catch all for handling errors
+// const { handleErrors } = require('./middleware/custom_errors');
+// app.use(handleErrors);
+
 //=============================================================================
 // START SERVER
 //=============================================================================
 app.listen(app.get('port'), () => {
-	console.log(`✅ PORT: ${app.get('port')} 🌟`);
+	console.log(`✅ Listening on port ${app.get('port')}`);
 });
