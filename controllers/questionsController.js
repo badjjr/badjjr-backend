d0ca6bf2e25ae00db708212d9a412d772a54023c
rhsagar145 <>
2022-07-11 15:25:51 -0400

    seed.json and quiz schema

---
+++ controllers/questionsController.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const Question = require('../models/Question');
+
+// INDEX
+// GET /api/Questions
+router.get('/', (req, res, next) => {
+	Question.find()
+		.then((Questions) => res.json(Questions))
+		.catch(next);
+});
+
+// SHOW
+// GET /api/Questions/:id
+router.get('/:id', async (req, res, next) => {
+	try {
+		const Question = await Question.findById(req.params.id);
+		res.json(Question);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// CREATE
+// POST /api/Questions
+router.post('/', async (req, res, next) => {
+	try {
+		const newQuestion = await Question.create(req.body);
+		Question.find({}).then((Questions) => {
+			// Send back 201 Created and the Questions list with the newly add Question
+			return res.status(201).json(Questions);
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+// UPDATE
+// PUT /api/Questionzes/:id
+router.put('/:id', async (req, res, next) => {
+	try {
+		const updateQuestion = await Question.findByIdAndUpdate(
+			req.params.id,
+			req.body
+		);
+		Question.find({}).then((Questions) => {
+			return res.json(Questions);
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+// DESTROY
+// DELETE /api/Questionzes/:id
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const deletedQuestion = await Question.findByIdAndDelete(req.params.id);
+		Question.find({}).then((Questions) => {
+			return res.json(Questions);
+		});
+	} catch (err) {
+		next(err);
+	}
+});
+
+module.exports = router;
