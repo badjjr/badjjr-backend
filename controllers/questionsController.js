194c750db5386b34f2b58c426bcc0344f19effbf
Jalynn <jalynnking@gmail.com>
2022-07-12 11:11:52 -0400

    Add comments to routes

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -7,13 +7,18 @@ const Quiz = require('../models/Quiz');
 // POST /api/questions
 router.post('/', async (req, res) => {
 	try {
+        //get body of new question
 		const newQuestion = req.body;
+        //get id of quiz to add question to
 		const quizId = req.body.quizId;
 		const findQuiz = await Quiz.findById(quizId)
 			.then((quiz) => {
+                //push the question to the quiz
 				quiz.questions.push(newQuestion);
+                //update quiz
 				return quiz.save();
 			})
+            //return updated quiz
 			.then((updatedQuiz) => res.status(201).json(updatedQuiz));
 	} catch (err) {
 		return res.sendStatus(400);
@@ -39,6 +44,7 @@ router.patch('/:id', async (req, res) => {
 				questionToUpdate.set(updatedQuestion);
 				return quiz.save();
 			})
+            //return updated quiz
 			.then((updatedQuiz) => res.status(201).json(updatedQuiz));
 	} catch (err) {
 		return res.sendStatus(400);
@@ -49,12 +55,17 @@ router.patch('/:id', async (req, res) => {
 // DELETE /api/questions/:id
 router.delete('/:id', async (req, res, next) => {
 	try {
+        //get id of question to be deleted
 		const questionId = req.params.id;
+        //Find quiz that contains the question id to be deleted
 		const deletedQuestion = await Quiz.findOne({ 'questions._id': questionId })
 			.then((quiz) => {
+                //delete the question
 				quiz.questions.id(questionId).remove();
+                //save the quiz
 				return quiz.save();
 			})
+            //return updated quiz
 			.then((updatedQuiz) => res.status(200).json(updatedQuiz));
 	} catch (err) {
 		return res.sendStatus(400);
